30b00f2483a4d881a84120ef180a4f4be5bb2fc7
Barret-Pierre <80763730+Barret-Pierre@users.noreply.github.com>
2021-12-09 00:54:54 +0100

    Modif Html / Modif Js

--- tyche_web/js/index.js
+++ tyche_web/js/index.js
@@ -1,8 +1,14 @@
 const proposer = document.getElementById("analyser");
+const inputSlide = document.querySelectorAll(".container-item>input");
+
+// variables
+let arrayClient = [];
+let salaire = 0;
+let economie;
 
 //rentrée
 
-let salaire = document.getElementById("salaire");
+let salaireInput = document.getElementById("salaire");
 
 //charge fixe
 let loyer = document.getElementById("loyer");
@@ -15,20 +21,69 @@ let abonnement = document.getElementById("abonnement");
 let course = document.getElementById("course_alimentaire");
 let loisir = document.getElementById("loisir");
 
+// crée un tableau qui contient l'ensemble des inputs si tout les input sont rempli et si le salaire est suppérieur ou égale aux dépenses
+function createTableauInput() {
+    if(verifInput() == true) {
+        if(verifSalaire() == true){
+            arrayClient=[];
+            arrayClient.push(
+                {name: salaireInput.id, price: salaireInput.value},
+                {name: loyer.id, price: loyer.value},
+                {name: elec_gaz.id, price: elec_gaz.value},
+                {name: elec_gaz.id, price: elec_gaz.value},
+                {name: assu_voiture.id, price: assu_voiture.value},
+                {name: abonnement.id, price: abonnement.value},
+                {name: course.id, price: course.value},
+                {name: loisir.id, price: loisir.value}
+            );
+            console.log(arrayClient);
+        }else{
+            alert("Votre salaire est inférieur à vos dépenses");
+        }
+    }else{
+        alert("Une de vos données est négative ou n'est pas valide");
+    }
+}
+
+// vérifie si chaque input rentré est bien un nombre positif
+function verifInput() {
+    let masque = /^[\d]+$/;
+    let compteur = 0;
+    for (let input of inputSlide){
+        if(masque.test(input.value)){
+            compteur++; 
+        }  
+    }
+    if(compteur == inputSlide.length){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// verifie si le salaire est bien supéreiur à l'ensemble des charges et met à jour la variable economie
+function verifSalaire() {
+    let somme = 0;
+    for (let input of inputSlide){
+        if(input.id == "salaire"){
+            salaire = parseInt(input.value, 10);
+        } else {
+            somme += parseInt(input.value, 10);
+        }
+    }
+    economie = salaire - somme;
+    console.log(economie);
+    if(Math.sign(economie) == -1){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 //array contient toute les entrées
 
 proposer.addEventListener("click", (e) => {
     e.stopPropagation();
-    let arrayClient = [];
-    arrayClient.push(
-        { name: salaire.id, price: salaire.value },
-        { name: loyer.id, price: loyer.value },
-        { name: elec_gaz.id, price: elec_gaz.value },
-        { name: elec_gaz.id, price: elec_gaz.value },
-        { name: assu_voiture.id, price: assu_voiture.value },
-        { name: abonnement.id, price: abonnement.value },
-        { name: course.id, price: course.value },
-        { name: loisir.id, price: loisir.value }
-    );
-    console.log(arrayClient);
+    createTableauInput();
 });
